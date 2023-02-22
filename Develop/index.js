7143db021feb08bea673667c4dfcdf63be5b8914
hopemansfield <hopemansfield@gmail.com>
2023-02-22 15:20:07 -0500

    going in a different direction with generateMarkdown as my code was not giving me the results I wanted

--- Develop/index.js
+++ Develop/index.js
@@ -1,183 +1,94 @@
 // TODO: Include packages needed for this application
 const inquirer = require("./utils/node_modules/inquirer");
 const fs = require("fs");
-// const generateMarkdown = require('./utils/generateMarkdown');
-// const markdown = require(generateMarkdown);
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
-  "Title of your project:",
-  "Your motivation for creating this app was to:",
-  "Now users of your app are able to:",
-  "While bulding your app you learned:",
-  "Something that stands out about your app is:",
-  "To install this app:",
-  "To use this app:",
-  "If you would like to include an image of your app in use, please provide a full path route or link to your image now:",
-  "Please provide any details about who you collaborated with on this app:",
-  "Please provide any details about third party assets that you used:",
-  "What type of license would you like to give this app?",
-  "Your GitHub username is:",
+  {
+    type: "input",
+    message: "Title of your project:",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Your motivation for creating this app was to:",
+    name: "motive",
+  },
+  {
+    type: "input",
+    message: "Now users of your app are able to:",
+    name: "solution",
+  },
+  {
+    type: "input",
+    message: "While bulding your app you learned:",
+    name: "lessons",
+  },
+  {
+    type: "input",
+    message: "Something that stands out about your app is:",
+    name: "standouts",
+  },
+  {
+    type: "input",
+    message: "To install this app:",
+    name: "install",
+  },
+  {
+    type: "input",
+    message: "To use this app:",
+    name: "use",
+  },
+  {
+    type: "input",
+    message:
+      "If you would like to include an image of your app in use, please provide a full path route or link to your image now:",
+    name: "image",
+  },
+  {
+    type: "input",
+    message:
+      "Please provide any details about who you collaborated with on this app:",
+    name: "collabs",
+  },
+  {
+    type: "input",
+    message:
+      "Please provide any details about third party assets that you used:",
+    name: "assets",
+  },
+  {
+    type: "list",
+    message: "What type of license would you like to give this app?",
+    name: "license",
+    choices: ["MIT", "Apache", "GPLv2", "GPLv3", "Other", "No License"],
+  },
+  {
+    type: "input",
+    message: "Your GitHub username is:",
+    name: "contact",
+  },
 ];
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: questions[0],
-      name: "title",
-    },
-    {
-      type: "input",
-      message: questions[1],
-      name: "motive",
-    },
-    {
-      type: "input",
-      message: questions[2],
-      name: "solution",
-    },
-    {
-      type: "input",
-      message: questions[3],
-      name: "lessons",
-    },
-    {
-      type: "input",
-      message: questions[4],
-      name: "standouts",
-    },
-    {
-      type: "input",
-      message: questions[5],
-      name: "install",
-    },
-    {
-      type: "input",
-      message: questions[6],
-      name: "use",
-    },
-    {
-        type: "input",
-        message: questions[7],
-        name: "image",
-    },
-    {
-      type: "input",
-      message: questions[8],
-      name: "collabs",
-    },
-    {
-      type: "input",
-      message: questions[9],
-      name: "assets",
-    },
-    {
-      type: "checkbox",
-      message: questions[10],
-      name: "license",
-      choices: ["MIT", "Apache", "GPLv2", "GPLv3", "Other", "No License"],
-    },
-    {
-      type: "input",
-      message: questions[11],
-      name: "questions",
-    },
-  ])
-  .then((response) => {
-    console.log("your response", response);
-
-    const {
-      title,
-      motive,
-      solution,
-      lessons,
-      standouts,
-      install,
-      use,
-      image,
-      collabs,
-      assets,
-      questions,
-      license
-    } = response;
-
-    const newReadMe = 
-`# ${title}
-
-## Table of Contents
-    
- <ul>
- <li>
-    <a href="#Description">Description</a>
-  </li>
-  <li>
-    <a href="#Installation">Installation</a> 
-  </li>
-  <li>
-    <a href="#Usage">Usage</a>
-  </li>
-  <li>
-    <a href="#Credits">Credits</a>
-  </li>
-  <li>
-    <a href="#Questions">Questions</a>
-  </li>
-  <li>
-  <a href="#License">License</a>
-</li>
-    </ul>
-    
-## Description <span id="Description">
-
-    The motivation behind the creation of ${title} was ${motive}
-    Now, users of this app can ${solution}
-    During the bulding process I learned ${lessons}
-    Something that I think stands out about this app is ${standouts}
-
-## Installation <span id="Installation">
-
-    To install ${title}:
-    
-    ${install}
-    
-## Usage <span id="Usage">
-
-    To use ${title}:
-    
-    ${use}
-
-<img src="${image}"/>
-    
-## Credits <span id="Credits">
-
-    ${collabs}
-    ${assets}
-
-## Questions <span id="Questions">
-    
-    To ask questions about this app, please contact me at:
-<a href="https://github.com/${questions}">github.com/${questions}</a>
-    
-## License <span id="License">
-
-    ${license}`;
-    
-
-    fs.writeFile("README.md", newReadMe, (err) => {
-      if (err) {
-        console.log("Something went wrong", err);
-      } else {
-        console.log("README has been created");
-      }
-    });
-  });
-
 // TODO: Create a function to write README file
-// function writeFile(fileName, data) {}
+function writeFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+   console.log("Your file has been created!")
+});
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+
+  const userInput = await inquirer.prompt(questions);
+  
+  writeFile("README.md", generateMarkdown(userInput), console.log(userInput));
+
+};
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,123 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+  if(license !== null){
+    return `##License
+    
+    This app has the following license:
+    
+    `;
+  }
+//below is my original code idea that was scrapped
+// const {license} = userInput
+//   let licenseChoice = {
+//     MIT: `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+//   };
+//   const {MIT} = licenseChoice;
+//   if(license === 'MIT'){
+//     licenseBadge = MIT;
+//   } else {
+//     return '';
+//   };
+// renderLicenseLink(licenseBadge);
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+
+
+//scrapped code
+// if(licenseBadge === MIT){
+//   licenseLink = `https://opensource.org/licenses/MIT`;
+// } else{
+//   return '';
+// };
+// renderLicenseSection(licenseBadge, licenseLink);
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+
+
+  //scrapped code
+// let licenseSection = 
+// `## License <span id="License">
+// ${licenseBadge}
+// ${licenseLink}
+// `;
+// generateMarkdown(licenseSection);
+//store the template literal in a variable?
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+return `## Table of Contents
+    
+ <ul>
+ <li>
+    <a href="#Description">Description</a>
+  </li>
+  <li>
+    <a href="#Installation">Installation</a> 
+  </li>
+  <li>
+    <a href="#Usage">Usage</a>
+  </li>
+  <li>
+    <a href="#Credits">Credits</a>
+  </li>
+  <li>
+    <a href="#Questions">Questions</a>
+  </li>
+  <li>
+  <a href="#License">License</a>
+</li>
+    </ul>
+    
+## Description <span id="Description">
+
+    The motivation behind the creation of ${data.title} was ${data.motive}
+    Now, users of this app can ${data.solution}
+    During the bulding process I learned ${data.lessons}
+    Something that I think stands out about this app is ${data.standouts}
+
+## Installation <span id="Installation">
+
+    To install ${data.title}:
+    
+    ${data.install}
+    
+## Usage <span id="Usage">
+
+    To use ${data.title}:
+    
+    ${data.use}
+
+<img src="${data.image}"/>
+    
+## Credits <span id="Credits">
+
+    ${data.collabs}
+    ${data.assets}
+
+## Questions <span id="Questions">
+    
+    To ask questions about this app, please contact me at:
+<a href="https://github.com/${data.contact}">github.com/${data.contact}</a>
+    
+## License <span id="License">
+
+    ${data.license}
+    
+    `;
 }
+   
 
 module.exports = generateMarkdown;
